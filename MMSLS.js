395f8b9cb2a40a2906a6e9f127816b19dba2292b
ChaselJace <Lvsshj@163.com>
2024-03-19 23:34:47 +0800

    Update MMSLS.js
    240319

--- MMSLS.js
+++ MMSLS.js
@@ -20,8 +20,8 @@ const MMSLS = [
  */
 const MMSLS = [
     
-    "https://www.maimemo.com/share/page?uid=14947368&pid=6f5f4f9709a3c370719d0a9b72d6dcab&tid=a492ad8f127d61a31ea5cb5684f04822",
-    "https://www.maimemo.com/share/page?uid=34553784&pid=6f5f4f9709a3c370719d0a9b72d6dcab&tid=b4e49b8c9e27121011b17d2e4a9afe45",
+    "https://www.maimemo.com/share/page?uid=14947368&pid=13674b834b884a0e0e20b6c547fe72ec&tid=823a0e1c299dfab0e2ad74043792932f",
+    "https://www.maimemo.com/share/page?uid=34553784&pid=13674b834b884a0e0e20b6c547fe72ec&tid=4d2e5f155dc57f7d074c1ba40d6e31f3",
 
   ]
   /**
